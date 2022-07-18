1ec74cbe8c7257e00a5d2ce0cc21adeba8d8ae9e
Jack Steiner <steiner.jack01@gmail.com>
2022-07-17 20:44:54 -0500

    added newemployee function, and got it on the table

--- scripts/client.js
+++ scripts/client.js
@@ -3,5 +3,30 @@ $(readyNow);
 
 function readyNow() {
     console.log('ready now');
-    
+    $('#submit-button').on('click', addNewEmployee);
+
 }
+
+function addNewEmployee () {
+    let firstName = $('#first-name-input').val();
+    console.log(firstName);
+    let lastName = $('#last-name-input').val();
+    console.log(lastName);
+    let id = $('#ID-input').val();
+    console.log(id);
+    let title = $('#title-input').val();
+    console.log(title);
+    let annualSalary = $('#salary-input').val();
+    console.log(annualSalary);
+
+    $('#employee-table').append(`
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${id}</td>
+        <td>${title}</td>
+        <td>${annualSalary}</td>
+    `)
+}
+
+totalSalary = 0;
+    for (let )
